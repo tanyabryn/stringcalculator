2a3b5d74beb75d26277bc0cc7324e81a1546de5b
Tanya Bra Brynjarsdottir <tanya14@i-7f3b5dd9.csd07cloud.internal>
2015-10-17 18:35:26 +0000

    Calculation with different delimiter added.

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -7,24 +7,36 @@ public class Calculator {
 			return 0;
 		}
 		else if(text.contains(",")|| text.contains("\n")){
+			if(text.substring(0, 2).equals("//"))
+			{
+				return sum(splitDelimeters(text));
+			}
 			return sum(splitNumbers(text));
 		}
 		else
 			return 1;
 	}
-
+	
 	private static int toInt(String number){
 		return Integer.parseInt(number);
 	}
+	
+	private static String[] splitDelimeters(String numbers){
+		int n = numbers.indexOf("\n");
+		String delimeter = numbers.substring(2, n);
+		String toSplit = numbers.substring(n+1);
+		
+		return toSplit.split(delimeter);
+	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split("[,\n]");
-	}
-      
-    private static int sum(String[] numbers){
- 	    int total = 0;
-        for(String number : numbers){
-		    total += toInt(number);
+	    return numbers.split("[\n,]");
+        }
+
+   	private static int sum(String[] numbers){
+ 		int total = 0;
+        	for(String number : numbers){
+			total += toInt(number);
 		}
 		return total;
     }
